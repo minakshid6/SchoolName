f3d40c48eb3791c839835cc9e8d6230d790b239a
minakshid6 <104327664+minakshid6@users.noreply.github.com>
2022-04-27 23:55:02 -0500

    by minu school

---
+++ school name/src/FisdSchoolName.java
@@ -0,0 +1,12 @@
+
+public class FisdSchoolName {
+
+	public static void main(String[] args) {
+		System.out.println("this is spark elementry");
+		System.out.println("this is spear elementry");
+		System.out.println("this is allen elementry");
+		System.out.println("this is pearson elementry");
+
+	}
+
+}
